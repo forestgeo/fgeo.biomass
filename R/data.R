69146a27311fb756f865fc5ed1c380d29f67919b
maurolepore <maurolepore@gmail.com>
2018-11-13 16:29:50 -0500

    Use data from SCBI.

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Data from SCBI.
+#'
+#' @name scbi
+#'
+#' @source http://bit.ly/scbi-forestgeo-data
+#' @format Objects of class tbl_df (inherit from tbl, data.frame). See structure
+#'   in __Examples__.
+#'
+#' @family census datasets
+#'
+#' @examples
+#' str(scbi_tree1)
+#'
+#' str(scbi_species)
+NULL
+
+#' @rdname scbi
+"scbi_tree1"
+#' @rdname scbi
+"scbi_species"
+
+
+
