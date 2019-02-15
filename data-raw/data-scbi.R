046c0dda92de6cb3207b4d14bb9f07e6a5347d1f
Mauro Lepore <maurolepore@gmail.com>
2019-02-15 11:51:11 -0500

    Use tor

--- data-raw/data-scbi.R
+++ data-raw/data-scbi.R
@@ -2,10 +2,7 @@ library(tibble)
 
 # Source: https://github.com/SCBI-ForestGEO/SCBI-ForestGEO-Data/tree/master/tree_main_census/data
 
-"data-raw/data-scbi" %>%
-  fgeo.tool::rdata_list() %>%
-  purrr::map(as_tibble) %>%
-  list2env(.GlobalEnv)
+tor::load_rdata("data-raw/data-scbi")
 
 # Rename to follow guidelines to documenting ForestGEO datasets:
 # https://forestgeo.github.io/fgeo.data/articles/document_data.html
