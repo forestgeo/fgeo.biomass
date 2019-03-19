958bb21e25c9646b31478128c5bcad9c8bfd962c
Mauro Lepore <maurolepore@gmail.com>
2019-03-19 15:19:50 -0400

    Remove fixme_find_duplicated_rowid(). No longer needed.

--- tests/testthat/test-fixme_units.R
+++ tests/testthat/test-fixme_units.R
@@ -1,6 +1,6 @@
 context("fixme_units")
 
-test_that("fixme_test returns expected value", {
+test_that("fixme_units returns expected value", {
   expect_equal(
     fixme_units(c("g", "kg", "lb", "Mg", "t")),
     c("g", "kg", "lbs", "Mg", "metric_ton")
