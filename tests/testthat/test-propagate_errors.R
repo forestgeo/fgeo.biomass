5980a44624dc26dd420450b11392c7ed6304a7f1
Mauro Lepore <maurolepore@gmail.com>
2019-04-17 10:31:36 -0400

    Use bciex for tests

--- tests/testthat/test-propagate_errors.R
+++ tests/testthat/test-propagate_errors.R
@@ -2,6 +2,25 @@ context("propagate_errors")
 
 library(dplyr)
 
+test_that("propagate_errors is sensitive to `Dpropag` or similar", {
+  data <- fgeo.biomass::scbi_stem_tiny_tree
+  species <- fgeo.biomass::scbi_species
+
+  biomass <- add_tropical_biomass(
+    data = data,
+    species = species,
+    latitude = 4,
+    longitude = -52
+  )
+
+  expect_is(
+    out <- propagete_errors(biomass, n = 50),
+    "list"
+  )
+  expect_false(is.null(out))
+  expect_false(all(is.na(out$AGB_simu)))
+})
+
 test_that("propagate_errors with lat, long returns a valid list", {
   data <- fgeo.biomass::scbi_stem_tiny_tree
   species <- fgeo.biomass::scbi_species
@@ -25,6 +44,8 @@ test_that("propagate_errors with lat, long returns a valid list", {
 })
 
 test_that("propagate_errors with lat, long and bci data returns a valid list", {
+  skip_if_not_installed("bciex")
+
   data <- bciex::bci12t7mini %>%
     as_tibble() %>%
     filter(!is.na(dbh)) %>%
